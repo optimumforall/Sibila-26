6dc6b68a09976f6b7bf04ba0bc6add58749611ca
Optimum for All <optimum.for.all@gmail.com>
2026-06-22 20:29:20 +0200

    Add files via upload

--- Sibila 26/next.config.js
+++ Sibila 26/next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['fluent-ffmpeg']
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/api/landing',
-        permanent: false,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
